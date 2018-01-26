14e41c9a37b43c4838158b420c9c6a24b8577b72
jrrubalc <jason.ray.rubalcaba@gmail.com>
2018-01-25 19:41:31 -0800

    fix date function

--- script.js
+++ script.js
@@ -1,75 +1,57 @@
-// let tasks = [
-//     {
-//         task_name: 'Clean Dishes',
-//         task_is_done: false,
-//         task_due: '2018-12-03',
-//     },
-//     {
-//         task_name: 'Buy a cat!',
-//         task_is_done: false,
-//         task_due: '2018-06-03',
-//     },
-//     {
-//         task_name: 'Propose to Katy Perry',
-//         task_is_done: true,
-//         task_due: '2018-05-05',
-//     },
-//     {
-//         task_name: 'Start eating two apples a day.',
-//         task_is_done: false,
-//         task_due: '2018-11-20',
-//     },
-//     {
-//         task_name: 'Clean kitty litter.',
-//         task_is_done: false,
-//         task_due: '2016-05-13',
-//     },
-// ];
-
-
-
 $(function () {
+
     let tasks = getLocalStorage();
 
-    $('.input').keypress(function (e) {
-        if (e.which == 13) {
-            $('form#login').submit();
-        }
-    });
-
-    $('.datepicker').pickadate({
-        selectMonths: true, // Creates a dropdown to control month
-        selectYears: 15, // Creates a dropdown of 15 years to control year,
-        today: 'Today',
-        clear: 'Clear',
-        close: 'Ok',
-        closeOnSelect: false // Close upon selecting a date,
-    });
-
+    setUpDatePicker();
     writeList();
     doneButtons();
+    addNewItem();
 
-
-
-
-function checkLate(date) {
-    var today = new Date();
-    var date_array = date.split("-");
-    date_array = date_array.map(function (x) {
-        return parseInt(x);
-    })
-    var js_date = new Date(date_array[0], date_array[1] - 1, date_array[2] + 1);
-    console.log(date_array[2] + 1);
-    if (today > js_date) {
-        return true;
+    function setLocalStorage(state) {
+        localStorage.setItem("state", JSON.stringify(state))
     }
-    return false;
-}
 
-function writeList(){
-    let finished = [];
-    for (var i = 0; i < tasks.length; i++) {
-        let newElem = $(`<li class="collection-item">
+    function getLocalStorage() {
+        if (localStorage.getItem("state") === null) {
+            return []
+        }
+        return JSON.parse(localStorage.getItem("state"))
+    }
+
+    //Check if an item is Late.
+    function checkLate(date) {
+        let today = new Date();
+        let month = ['January',
+            'February',
+            'March',
+            'April',
+            'May',
+            'June',
+            'July',
+            'August',
+            'September',
+            'October',
+            'November',
+            'December']
+
+        date = date.replace(",", "");
+        date = date.split(" ");
+        date[1] = month.indexOf(date[1]) //Change into number format
+
+        let jsDate = new Date(date[2], date[1], date[0])
+
+        if (today > jsDate) {
+            return true;
+        }
+        return false;
+    }
+
+
+    // Writes the tasks to the DOM
+    function writeList() {
+        let finished = [];
+        for (var i = 0; i < tasks.length; i++) {
+            let newElem = $(`<li class="collection-item">
         <div class="row nonono">
             <div class="col s4 myleft nonono">
                  ${tasks[i].task_name} 
@@ -84,81 +66,87 @@ function writeList(){
 
         </li>`);
 
-        if (checkLate(tasks[i].task_due)) {
-            newElem.addClass("late");
-        }
-        if (tasks[i].task_is_done) {
-            newElem.addClass("done");
-            newElem.removeClass("late")
-            finished.push(newElem);
-        } else {
-            $("#todo-list").append(newElem);
-        }
-    }
-    finished.forEach(function(elm){
-        $("#todo-list").append(elm);
-    })
-
-
-}
-
-function setLocalStorage(state){
-    localStorage.setItem("state", JSON.stringify(state))
-}
-
-function getLocalStorage(){
-    if(localStorage.getItem("state") === null){
-        return []
-    }
-    return JSON.parse(localStorage.getItem("state"))
-}
-
-function doneButtons(){
-    $(".btn-done").click(function(){
-        $(this).html("<i class='material-icons left'>delete_forever</i> Remove")
-        let thisElm = $(this).parent().parent().parent();
-        let itemToRemove = thisElm.text().trim().split(" ")[0];
-        let itemsLoc = tasks.findIndex(function(item){
-            if(item.task_name === itemToRemove){
-                return true
+            if (checkLate(tasks[i].task_due)) {
+                newElem.addClass("late");
             }
-            return false
+            if (tasks[i].task_is_done) {
+                newElem.addClass("done");
+                newElem.removeClass("late")
+                finished.push(newElem);
+            } else {
+                $("#todo-list").append(newElem);
+            }
+        }
+        finished.forEach(function (elm) {
+            $("#todo-list").append(elm);
         })
-        tasks[itemsLoc].task_is_done = true;
-        setLocalStorage(tasks);
-        $(this).parent().parent().parent().remove();
-        thisElm.addClass("done");
-        $("#todo-list").append(thisElm);
-        $(".done .btn").click(function(){
+
+
+    }
+
+
+
+    function setUpDatePicker() {
+        $('.datepicker').pickadate({
+            selectMonths: true, // Creates a dropdown to control month
+            selectYears: 15, // Creates a dropdown of 15 years to control year,
+            today: 'Today',
+            clear: 'Clear',
+            close: 'Ok',
+            closeOnSelect: false // Close upon selecting a date,
+        });
+    }
+
+
+
+
+    function doneButtons() {
+        $(".btn-done").click(function () {
+            $(this).html("<i class='material-icons left'>delete_forever</i> Remove")
+            let thisElm = $(this).parent().parent().parent();
+            let itemToRemove = thisElm.text().trim().split(" ")[0];
+            let itemsLoc = tasks.findIndex(function (item) {
+                if (item.task_name === itemToRemove) {
+                    return true
+                }
+                return false
+            })
+            tasks[itemsLoc].task_is_done = true;
+            setLocalStorage(tasks);
             $(this).parent().parent().parent().remove();
+            thisElm.addClass("done");
+            $("#todo-list").append(thisElm);
+            $(".done .btn").click(function () {
+                $(this).parent().parent().parent().remove();
+            })
         })
-    }) 
-}
+    }
 
-$(".adder").click(function(event) {
-        let name = $("#todoname").val();
-        let date = $("#tododate").val();
-        $("form")[0].reset();
-        $("#todo-list").prepend(`<li class="collection-item">
-        <div class="row nonono">
-            <div class="col s4 myleft nonono">
-                 ${name} 
-            </div>
-            <div class="col s4 mycenter nonono">
-                ${date}
-            </div>
-            <div class="col s4 myright nonono">
-              <a class="waves-effect waves-light btn btn-done"><i class="material-icons left">delete</i>Done</a>
-             </div>
-        </div>
-        </li>`);
-        tasks.push({
-            task_name: name,
-            task_is_done: false,
-            task_due: date,
-        })
-        doneButtons();
-        setLocalStorage(tasks);
-});
-
+    function addNewItem() {
+        $(".adder").click(function (event) {
+            let name = $("#todoname").val();
+            let date = $("#tododate").val();
+            $("form")[0].reset();
+            $("#todo-list").prepend(`<li class="collection-item">
+                                        <div class="row nonono">
+                                            <div class="col s4 myleft nonono">
+                                                ${name} 
+                                            </div>
+                                            <div class="col s4 mycenter nonono">
+                                                ${date}
+                                            </div>
+                                            <div class="col s4 myright nonono">
+                                            <a class="waves-effect waves-light btn btn-done"><i class="material-icons left">delete</i>Done</a>
+                                            </div>
+                                        </div>
+                                    </li>`);
+            tasks.push({
+                task_name: name,
+                task_is_done: false,
+                task_due: date,
+            })
+            doneButtons();
+            setLocalStorage(tasks);
+        });
+    }
 })
